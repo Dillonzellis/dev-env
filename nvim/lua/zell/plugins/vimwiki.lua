34d4e9367f6197f23bb1658713a34a781e974a16
dillonzellis <dillonzellis@gmail.com>
2023-10-01 23:28:50 -0400

    fix remove vimwiki git keymaps

--- nvim/lua/zell/plugins/vimwiki.lua
+++ nvim/lua/zell/plugins/vimwiki.lua
@@ -4,11 +4,6 @@ return {
 	config = function()
 		-- Any vimwiki specific configuration/setup...
 
-		-- Create custom command GCommitPush
-		vim.cmd([[
-    command! GCommitPush execute 'silent !git add . && git commit -m "'.shellescape(system("date '+%Y-%m-%d %H:%M:%S'"), 1).'" && git push'
-    ]])
-
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
@@ -17,8 +12,5 @@ return {
 
 		-- Map <leader>ll to Vimwiki command
 		keymap.set("n", "<leader>ll", "<cmd>VimwikiIndex<cr>", { desc = "Open Vimwiki" })
-
-		-- Map <leader>g to the GCommitPush command
-		keymap.set("n", "<leader>-", "<cmd>GCommitPush<cr>", { desc = "Git commit and push with timestamp" })
 	end,
 }
