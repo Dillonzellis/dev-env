4c51a1757bf4e3ee9edb1880a3aa651f3a7f4504
dillonzellis <dillonzellis@gmail.com>
2023-10-01 23:21:54 -0400

    '2023-10-01 23:21:54 '

--- aliases.sh
+++ aliases.sh
@@ -1,19 +1,24 @@
+# Define DEV_PATH constant
+DEV_PATH="~/.dev-env"
+
 # config
 
-# alias nrc="cd ~/.config/nvim"
-# alias so="source ~/.zshrc"
-alias alrc="cd ~/commonrc"
+alias dev="cd $DEV_PATH"
+alias so="source $DEV_PATH/.zshrc"
 
 # utilities
 
-alias nvim='XDG_CONFIG_HOME=~/.dev-env nvim'
-alias vi='XDG_CONFIG_HOME=~/.dev-env nvim'
+alias nvim="XDG_CONFIG_HOME=$DEV_PATH nvim"
+alias vi="XDG_CONFIG_HOME=$DEV_PATH nvim"
+
+# vimwiki git
+alias notepush="git add . && git commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\" && git push"
 
 alias rf="rm -rf"
 alias lash="ls -lash"
 # alias cl="clear"
 alias gs="git status"
-alias tmux='tmux -f ~/.dev-env/.tmux.conf'
+alias tmux="tmux -f $DEV_PATH/.tmux.conf"
 
 # yabai
 alias startyab="yabai --start-service"
@@ -25,25 +30,23 @@ alias nlday="nightlight temp 45"
 alias nlnight="nightlight temp 100"
 
 # directory aliases
-
 alias desktop="cd ~/Desktop"
 alias prj="cd ~/Projects"
 alias dl="cd ~/Downloads"
 alias gbd="cd ~/Projects/gbd"
 alias temp="cd ~/Projects/temp"
-alias port="cd ~/Projects/dillonzellis-portfolio/"
+alias notes="cd ~/vimwiki"
 
 # project directories
 alias smcu="cd ~/Projects/gbd/small-cu/"
+alias port="cd ~/Projects/dillonzellis-portfolio/"
 alias ecom="cd ~/Projects/ecommerce-admin/"
 alias mood="cd ~/Projects/mood"
 alias fpic="cd ~/Projects/family-pic/"
 
 # gbd directories
-# BASEDIR="X:/Websites/activechanges/_MAIN"
 BASEDIR="~/Projects/gbd"
 
-# alias main="cd $BASEDIR"
 alias muts="cd $BASEDIR/muts"
 alias plat="cd $BASEDIR/Platinum-2018"
 alias cufs="cd $BASEDIR/CUFS-2018"
@@ -64,7 +67,3 @@ alias byte="cd $BASEDIR/byte-reality"
 alias lscu-about="cd $BASEDIR/lscu/LSCU-about"
 alias lscu-mem="cd $BASEDIR/lscu/LSCU-member-engagement"
 alias lscu-found="cd $BASEDIR/lscu/LSCU-foundation"
-
-# update the base directory for internal sites
-# BASEDIR="x/Websites/Internal Websites"
-# alias lscu="cd $BASEDIR/LSCU 2020"
--- nvim/lua/zell/plugins/vimwiki.lua
+++ nvim/lua/zell/plugins/vimwiki.lua
@@ -2,8 +2,12 @@ return {
 	"vimwiki/vimwiki",
 
 	config = function()
-		-- You can place any vimwiki setup/configuration here, similar to the telescope.setup({})
-		-- If vimwiki has any lua-based setup, that is.
+		-- Any vimwiki specific configuration/setup...
+
+		-- Create custom command GCommitPush
+		vim.cmd([[
+    command! GCommitPush execute 'silent !git add . && git commit -m "'.shellescape(system("date '+%Y-%m-%d %H:%M:%S'"), 1).'" && git push'
+    ]])
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
@@ -11,8 +15,10 @@ return {
 		-- Unmap the default <leader>ww
 		vim.api.nvim_del_keymap("n", "<leader>ww")
 
-		-- Map <leader>ll to Vimwiki command.
-		-- I'm using ':VimwikiIndex<CR>' as an example, adjust if vimwiki uses a different command.
+		-- Map <leader>ll to Vimwiki command
 		keymap.set("n", "<leader>ll", "<cmd>VimwikiIndex<cr>", { desc = "Open Vimwiki" })
+
+		-- Map <leader>g to the GCommitPush command
+		keymap.set("n", "<leader>-", "<cmd>GCommitPush<cr>", { desc = "Git commit and push with timestamp" })
 	end,
 }
